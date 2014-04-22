fa6e768b67fa4c577cf1fa540d56941c0ee3f084
Copper Cat <djf00p@gmail.com>
2014-04-22 19:35:23 +0100

    yea

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ActiveRecord::Base
+  belongs_to :category
+
+  validate_presence_of :title, :content
+
+  scope :active, where('active = ?', true)
+  scope :alphabetical, order('name')
+end
